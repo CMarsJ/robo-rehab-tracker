6ef644697e35b63ecc83e38147b449c00a252eb0
Carlos Carreño J. <carjerez2002@gmail.com>
2025-08-23 20:38:09 -0500

    eliminacion mensaje console

--- src/components/DataSimulator.tsx
+++ src/components/DataSimulator.tsx
@@ -75,11 +75,6 @@ const DataSimulator = () => {
 
     updateSimulationData(newLeftHand, newRightHand);
     
-    console.log('Datos simulados enviados:', {
-      leftHand: newLeftHand,
-      rightHand: newRightHand
-    });
-    
     // Restaurar posición de scroll después de un breve delay
     setTimeout(() => {
       if (scrollRef.current) {
